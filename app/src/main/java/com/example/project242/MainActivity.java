24c13ed894d224a5575340b961e3f6026079f26c
Wakate <moody-8@live.com>
2021-11-04 01:01:55 +0400

    added my name2

--- app/src/main/java/com/example/project242/MainActivity.java
+++ app/src/main/java/com/example/project242/MainActivity.java
@@ -10,8 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        String name = "Maya was here";
-        String name2 = "Mudi was here";
     }
 
 }
