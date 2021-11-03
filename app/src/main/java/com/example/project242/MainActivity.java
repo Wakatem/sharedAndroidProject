b02651b5d96dbc2c413d0394f6e5852e6dac08dc
Wakate <moody-8@live.com>
2021-11-04 00:51:50 +0400

    added my name

--- app/src/main/java/com/example/project242/MainActivity.java
+++ app/src/main/java/com/example/project242/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String name = "Maya was here";
+        String name2 = "Wakate was here";
     }
 
 }
