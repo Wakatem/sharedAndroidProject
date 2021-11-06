9fd5f68fac4221c083fe84a323e68b7b0f3c7cfe
Wakate <moody-8@live.com>
2021-11-07 00:35:21 +0400

    Add sorting method to CostsHandler

--- app/src/main/java/com/example/project242/MonetaryRates/CostsHandler.java
+++ app/src/main/java/com/example/project242/MonetaryRates/CostsHandler.java
@@ -12,10 +12,71 @@ public class CostsHandler extends ArrayList<Cost> {
     }
 
 
+    private void sortCosts() {
+        for (int pos = 0; pos < size(); pos++) {
+
+            //if next index is not OutOfBounds
+            if (pos + 1 != size()) {
+                Cost currentCost = get(pos);
+                Cost nextCost = get(pos + 1);
+
+                //compare current Cost with next Cost
+                if (currentCost.getHours() < nextCost.getHours()) {
+                    set(pos, nextCost);          // replace
+                    set(pos + 1, currentCost);     // return current cost back to list
+
+                    //adjust index
+                    if (pos >= 1) {
+                        pos = pos - 2;
+                    }
+
+                }
+                //check both costs types (only if they got same duration)
+                else if (nextCost.getHours() == currentCost.getHours()) {
+
+                    if ((currentCost.getType() == Cost.Type.EQUAL) && (nextCost.getType() == Cost.Type.GREATER)) {
+                        set(pos, nextCost);            // replace
+                        set(pos + 1, currentCost);     // return current cost back to list
+
+                        //adjust index
+                        if (pos >= 1) {
+                            pos = pos - 2;
+                        }
+                    }
+                    if ((currentCost.getType() == Cost.Type.LESS) && (nextCost.getType() == Cost.Type.EQUAL)) {
+                        set(pos, nextCost);            // replace
+                        set(pos + 1, currentCost);     // return current cost back to list
+
+                    }
+
+                    if ((currentCost.getType() == Cost.Type.LESS) && (nextCost.getType() == Cost.Type.GREATER)) {
+                        set(pos, nextCost);            // replace
+                        set(pos + 1, currentCost);     // return current cost back to list
+
+                        //adjust index
+                        if (pos >= 1) {
+                            pos = pos - 2;
+                        }
+                    }
+
+
+                }
+
+
+            }//1st if
+
+
+        }//for
+
+
+    }
+
+
+
     @Override
     public boolean add(Cost cost) {
         boolean isAdded = super.add(cost);
-        //sortCosts();
+        sortCosts();
 
         return isAdded;
     }
